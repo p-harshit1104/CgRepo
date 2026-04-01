5195a54d4dadd91401af12b817cd898680c4cfbb
p-harshit1104 <harshitp110604@gmail.com>
2026-04-01 12:49:40 +0530

    added new entity

---
+++ LoanApplicationManagementSystem/src/main/java/com/cg/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.cg.entity;
+
+public class Customer {
+ private Integer roll;
+ private String name,email;
+ public Integer getRoll() {
+	return roll;
+ }
+ public void setRoll(Integer roll) {
+	this.roll = roll;
+ }
+ public String getName() {
+	return name;
+ }
+ public void setName(String name) {
+	this.name = name;
+ }
+ public String getEmail() {
+	return email;
+ }
+ public void setEmail(String email) {
+	this.email = email;
+ }
+ 
+}
